054333fd288b6185936c9269040822cd7ea7a039
Carlos Morales <CrlsMrls@outlook.com>
2024-05-22 11:30:38 +0200

    Fix issues

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,58 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import {
+  provideFirestore,
+  getFirestore,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
+import {
+  provideFunctions,
+  getFunctions,
+  connectFunctionsEmulator,
+} from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import {
+  provideStorage,
+  getStorage,
+  connectStorageEmulator,
+} from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     importProvidersFrom(
+//       provideFirebaseApp(() => initializeApp(environment.firebase)),
+//       provideFirestore(() => getFirestore()),
+//       provideAuth(() => getAuth()),
+//       provideFunctions(() => getFunctions()),
+//       provideStorage(() => getStorage()),
+//       provideMessaging(() => getMessaging())
+//     ),
+//     provideRouter(routes),
+//     provideFirebaseApp(() =>
+//       initializeApp({
+//         projectId: 'ai-playground-424107',
+//         appId: '1:219236925850:web:b97c3e92c66b7ac514fa23',
+//         storageBucket: 'ai-playground-424107.appspot.com',
+//         // locationId: 'europe-west3',
+//         apiKey: 'AIzaSyCXEAsw45mQ1ByJuvH19WTbqgxIlwydh38',
+//         authDomain: 'ai-playground-424107.firebaseapp.com',
+//         messagingSenderId: '219236925850',
+//       })
+//     ),
+//     provideAuth(() => getAuth()),
+//     provideFirestore(() => getFirestore()),
+//     provideMessaging(() => getMessaging()),
+//     provideStorage(() => getStorage()),
+//   ],
+// };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
+      // @ts-ignore
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
@@ -19,6 +61,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"ai-playground-424107","appId":"1:219236925850:web:b97c3e92c66b7ac514fa23","storageBucket":"ai-playground-424107.appspot.com","locationId":"europe-west3","apiKey":"AIzaSyCXEAsw45mQ1ByJuvH19WTbqgxIlwydh38","authDomain":"ai-playground-424107.firebaseapp.com","messagingSenderId":"219236925850"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes),
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,25 @@
+// export const environment = {
+//   firebase: {
+//     apiKey: 'API_KEY',
+//     authDomain: 'PROJECT_ID.firebaseapp.com',
+//     databaseURL: 'https://PROJECT_ID.firebaseio.com',
+//     projectId: 'PROJECT_ID',
+//     storageBucket: 'PROJECT_ID.appspot.com',
+//     messagingSenderId: 'SENDER_ID',
+//     appId: 'APP_ID',
+//     measurementId: 'G-MEASUREMENT_ID',
+//   },
+// };
+
+import { FirebaseApp } from '@angular/fire/app';
+
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: 'AIzaSyCXEAsw45mQ1ByJuvH19WTbqgxIlwydh38',
+    authDomain: 'ai-playground-424107.firebaseapp.com',
+    projectId: 'ai-playground-424107',
+    storageBucket: 'ai-playground-424107.appspot.com',
+    messagingSenderId: '219236925850',
+    appId: '1:219236925850:web:b97c3e92c66b7ac514fa23',
   },
 };
